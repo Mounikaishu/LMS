5834acce8352911e18f15e95777a378aaaaf5ee5
Mounikaishu <24b01a1275@svecw.edu.in>
2026-08-06 10:03:34 +0530

    updated config accordingly to new json updates

--- config.py
+++ config.py
@@ -1,18 +1,9 @@
-from pathlib import Path
 import os
-
-# ==============================
-# LMS Project Directory
-# ==============================
+from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent
 
-# ==============================
-# Repository Path
-# ==============================
-
 DEFAULT_REPOSITORY = Path(r"C:\repositories\college-materials")
-
 
 REPOSITORY = Path(
     os.getenv(
@@ -21,13 +12,12 @@ REPOSITORY = Path(
     )
 )
 
-# ==============================
-# Local Folders
-# ==============================
+GITHUB_OWNER = "Mounikaishu"
+GITHUB_REPO = "college-materials"
+GITHUB_BRANCH = "main"
 
 UPLOAD_FOLDER = BASE_DIR / "uploads"
 TEMP_FOLDER = BASE_DIR / "temp"
 
 UPLOAD_FOLDER.mkdir(exist_ok=True)
 TEMP_FOLDER.mkdir(exist_ok=True)
-print("Repository Path:", REPOSITORY)
